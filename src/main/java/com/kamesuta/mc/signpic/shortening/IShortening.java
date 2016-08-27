315ecce86d544a34425f9d4952e67cefa856f256
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-08-27 17:23:21 +0900

    Interface

--- src/main/java/com/kamesuta/mc/signpic/shortening/IShortening.java
+++ src/main/java/com/kamesuta/mc/signpic/shortening/IShortening.java
@@ -2,6 +2,8 @@ package com.kamesuta.mc.signpic.shortening;
 
 public interface IShortening {
 
-	public void shortening(String rawurl, IShorteningCallback callback, String getname);
+	public void shortening(String rawurl, IShorteningCallback callback);
+
+	public String getname();
 
 }
